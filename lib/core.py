0c3a0ee1da7c0cea986e6149fd516da56ad2ba12
Michal Knapík <knapikmichal94@gmail.com>
2018-11-05 22:14:50 +0100

    moisture measurement moved into core

--- lib/core.py
+++ lib/core.py
@@ -3,12 +3,23 @@ from LIS2HH12 import LIS2HH12
 from SI7006A20 import SI7006A20
 from LTR329ALS01 import LTR329ALS01
 from MPL3115A2 import MPL3115A2,ALTITUDE,PRESSURE
+from machine import Pin,ADC
 
 py = Pysense()
 mp = MPL3115A2(pysense=py,mode=ALTITUDE) # Returns height in meters. Mode may also be set to PRESSURE, returning a value in Pascals
 si = SI7006A20(pysense=py)
 lt = LTR329ALS01(pysense=py)
-acc = LIS2HH12(pysense=py)
+#acc = LIS2HH12(pysense=py)
+
+adc = ADC()
+apin = adc.channel(pin='P15',attn=ADC.ATTN_11DB)
+p_out = Pin('P9', mode = Pin.OUT, pull = Pin.PULL_DOWN)
+
+def moist_sensor():
+    p_out.value(1)
+    volts = apin.value()
+    p_out.value(0)
+    return volts / 4.096
 
 def measurements():
     global py, mp, si, lt, acc
@@ -22,4 +33,6 @@ def measurements():
     light = lt.light()
     #acc
     voltage = py.read_battery_voltage()
+    #moist = int(moist_sensor())
+
     return (temp_mp, temp_si), hum, light, press, voltage
--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 from network import Bluetooth
 import ubinascii
 import pycom
-from machine import Pin,ADC
 
 pycom.heartbeat(False)
 
-adc = ADC()
-apin = adc.channel(pin='P15',attn=ADC.ATTN_11DB)
-p_out = Pin('P9', mode = Pin.OUT, pull = Pin.PULL_DOWN)
-
-def moist_sensor():
-    p_out.value(1)
-    volts = apin.value()
-    p_out.value(0)
-    return volts / 4.096
-
-volts = int(moist_sensor())
-print(volts)
-
-'''
 bluetooth = Bluetooth()
 bluetooth.start_scan(20)
 while bluetooth.isscanning():
@@ -28,4 +13,3 @@ while bluetooth.isscanning():
         if mac == bytearray('b827ebeec52e'):
             name = bluetooth.resolve_adv_data(adv.data, Bluetooth.ADV_NAME_CMPL)
             print(mac, name, adv.rssi)
-'''
