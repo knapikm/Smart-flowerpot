4c31c867571bda9cf885e1f1c37108ca05218ff2
Michal Knapík <knapikmichal94@gmail.com>
2018-11-17 22:28:17 +0100

    new characteristic + calback for response from GATT client

--- lib/bleAPI.py
+++ lib/bleAPI.py
@@ -15,7 +15,7 @@ def conn_cb (bt_o):
 ​
 bluetooth.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=conn_cb)
 bluetooth.advertise(True)
-srv = bluetooth.service(uuid=4321, isprimary=True, nbr_chars=6, start=False)
+srv = bluetooth.service(uuid=4321, isprimary=True, nbr_chars=7, start=False)
 
 temp, hum, light, press, voltage, moist = measurements()
 tempChr = srv.characteristic(uuid=4561, value=str(temp))
@@ -24,7 +24,13 @@ lightChr = srv.characteristic(uuid=4563, value=str(light))
 pressChr = srv.characteristic(uuid=4564, value=str(press))
 voltChr = srv.characteristic(uuid=4565, value=str(voltage))
 moistChr = srv.characteristic(uuid=4566, value=str(moist))
+respChr = srv.characteristic(uuid=4567, value=0)
 srv.start()
+
+def char_read_cb_handler(chr):
+    events = chr.events()
+    if events & Bluetooth.CHAR_WRITE_EVENT:
+        print('write', chr.value())
 
 def char_cb_handler(chr):
     print('read', chr)
@@ -35,6 +41,7 @@ char3_cb = lightChr.callback(trigger=Bluetooth.CHAR_READ_EVENT, handler=char_cb_
 char4_cb = pressChr.callback(trigger=Bluetooth.CHAR_READ_EVENT, handler=char_cb_handler)
 char5_cb = voltChr.callback(trigger=Bluetooth.CHAR_READ_EVENT, handler=char_cb_handler)
 char6_cb = moistChr.callback(trigger=Bluetooth.CHAR_READ_EVENT, handler=char_cb_handler)
+char6_cb = respChr.callback(trigger=Bluetooth.CHAR_WRITE_EVENT, handler=char_read_cb_handler)
 
 '''
 bluetooth.start_scan(20)
